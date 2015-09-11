64bb001d16b0940d37eb0b869652e326a3217a03
casperthuis <casper.thuis@gmail.com>
2015-09-11 12:24:42 +0200

    Finaly some test

--- read_test_set.py
+++ read_test_set.py
@@ -1,17 +1,17 @@
 import os
-f = open('puzzles.txt', 'r')
+f = open('Assignment-1/puzzles.txt', 'r')
 
 index = 1
 a = [line for line in f]
 b = [line for i, line in enumerate(a) if i % 2 == 0]
 c = [line for i, line in enumerate(a) if i % 2 == 1]
 d = zip(b,c)
-#print d
+print len(d)
 
 for i  in d:
-	print i
+	#print i
 	location = i[1][:-1]
-	print location
+	#print location
 	#a.write(i[1])
 	if not os.path.exists(location):
 		os.makedirs(location)
