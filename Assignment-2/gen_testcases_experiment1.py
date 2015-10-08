4b2b12d48dafb8352a60281c0d431238a1cf720d
Minh Ngo <nlminhtl@gmail.com>
2015-10-08 13:28:07 +0200

    Test data for the first experiment.
    Comparison of Model 1 and Model 2

---
+++ Assignment-2/gen_testcases_experiment1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import copy
+fullSudoku = open('testFull.txt', 'r')
+board = next(fullSudoku)
+for i in range(1, 72):
+    for m in range(1000):
+        boardC = [board[k] for k in range(len(board) - 1)]
+        indexes = np.arange(len(board) - 1)
+        np.random.shuffle(indexes)
+        for j in range(i):
+            boardC[indexes[j]] = '.'
+        print(''.join(boardC))
